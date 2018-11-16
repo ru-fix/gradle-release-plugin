77b064f8f45423a59a81f487b188a4ea9b45b6dd
kasfandiyarov <kasfandiyarov@gmail.com>
2018-11-16 10:18:15 +0300

    Add extra checks

--- src/main/kotlin/ru/fix/gradle/release/plugin/BranchGardener.kt
+++ src/main/kotlin/ru/fix/gradle/release/plugin/BranchGardener.kt
@@ -115,8 +115,9 @@ class BranchGardener(private val project: Project) {
 
 
         if (git.getCurrentBranch() != extension.mainBranch) {
-            throw GradleException("You are not in the main branch: ${extension.mainBranch}.\n" +
+            project.logger.warn("You are not in the main branch: ${extension.mainBranch}.\n" +
                     "Release branch can be built only from ${extension.mainBranch} branch.")
+            return
         }
 
         val supposedVersion = versionManager.supposeBranchVersion()

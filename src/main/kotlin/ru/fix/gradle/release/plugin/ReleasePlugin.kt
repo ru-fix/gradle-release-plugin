c726bb11295c932cac8d1fd7b9c48f2f0de37381
kasfandiyarov <kasfandiyarov@gmail.com>
2019-06-20 10:36:30 +0300

    git repository search update

--- src/main/kotlin/ru/fix/gradle/release/plugin/ReleasePlugin.kt
+++ src/main/kotlin/ru/fix/gradle/release/plugin/ReleasePlugin.kt
@@ -5,11 +5,10 @@ import org.gradle.api.Project
 
 class ReleasePlugin : Plugin<Project> {
 
-
-    override fun apply(project: Project?) {
-        project?.extensions?.create("ru.fix.gradle.release", ReleaseExtension::class.java)
-        project?.tasks?.create("createRelease", CreateReleaseTask::class.java)
-        project?.tasks?.create("createReleaseBranch", CreateReleaseBranchTask::class.java)
+    override fun apply(project: Project) {
+        project.extensions.create("ru.fix.gradle.release", ReleaseExtension::class.java)
+        project.tasks.create("createRelease", CreateReleaseTask::class.java)
+        project.tasks.create("createReleaseBranch", CreateReleaseBranchTask::class.java)
     }
 
 
